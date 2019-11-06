8af1ad622092e574b159fdad7a711edbfbf4b038
MIGUEL OLIVEIRA <miguel.oliveira.2001@gmail.com>
2019-11-06 16:00:02 +0000

    remocao da funcao menu e main

--- calculadora.c
+++ calculadora.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 
 void soma(){
     float prim, seg, result=0;
@@ -17,61 +16,3 @@ void soma(){
     printf("O resultado e: %.2f\n", result);
 }
 
-
-void menu(){
-
-    int escolha;
-
-    printf("\t\tBem vindo a calculadora em C\n\n");
-
-    printf("Selecione uma operacao matematica:\n");
-    printf("\t1- Adicao\n");
-    printf("\t2- Subtracao\n");
-    printf("\t3- Divisao\n");
-    printf("\t4- Multiplicacao\n");
-    printf("\t5- Sair\n");
-    printf("Operacao: ");
-    scanf ("%d", &escolha);
-
-
-    switch (escolha){
-        case 1:
-            soma();
-            break;
-
-        case 2:
-            subtrai();
-            break;
-
-        case 3:
-            divide();
-            break;
-
-        case 4:
-            multiplica();
-            break;
-
-        case 5:
-            system("exit");
-            printf("\nFim...\n\n");
-            break;
-
-        default:
-            printf("\nComando invalido, tente novamente!\n\n");
-            system ("pause");
-            system ("cls");
-            menu();
-            break;
-    }
-}
-
-
-
-
-
-int main(){
-
-    menu();
-    system("pause");
-    return 0;
-}
