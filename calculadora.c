12432033376e5e14b3ee7986c9b3a5b6a33a5d30
Ricardocampos <ricardocampox@hotmail.com>
2019-11-06 15:42:58 +0000

    func_divisao

--- calculadora.c
+++ calculadora.c
@@ -1,40 +1,30 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
-void soma()
-{ 
-    //todo
-    system ("pause"); 
-    system("cls"); 
-    menu(); 
+void divide(){
+
+    float prim, seg, result=0;
+
+    printf("///////DIVISAO///////");
+
+    printf("Indique o 1 numero:\n");
+
+    scanf("%f", &prim);
+
+    printf("Indique o 2 numero:\n");
+
+    scanf("%f", &seg);
+
+    // Divide os valores
+
+    result= prim/seg;
+    printf("O resultado e: %.2f\n", result);
+
 }
 
-void subtrai()
-{
-    //todo
-    system ("pause");
-    system("cls"); 
-    menu();
-}
 
-void divide()
-{ 
-    //todo
-    system ("pause");
-    system("cls");
-    menu();
-}
+void menu(){
 
-void multiplica()
-{ 
-    //todo
-    system ("pause");
-    system("cls");
-    menu();
-}
-
-void menu()
-{ 
     int escolha;
 
     printf("\t\tBem vindo a calculadora em C\n\n");
@@ -48,19 +38,19 @@ void menu()
     printf("Operacao: ");
     scanf ("%d", &escolha);
 
-    switch (escolha)
-    {
-        case 1: 
-            soma(); 
-        break; 
+
+    switch (escolha){
+        case 1:
+            soma();
+            break;
 
         case 2:
             subtrai();
-        break;
+            break;
 
         case 3:
             divide();
-        break;
+            break;
 
         case 4:
             multiplica();
@@ -69,20 +59,23 @@ void menu()
         case 5:
             system("exit");
             printf("\nFim...\n\n");
-        break;
+            break;
 
-        default: 
+        default:
             printf("\nComando invalido, tente novamente!\n\n");
             system ("pause");
             system ("cls");
-            menu(); 
+            menu();
             break;
     }
 }
 
 
-int main() 
-{
+
+
+
+int main(){
+
     menu();
     system("pause");
     return 0;
